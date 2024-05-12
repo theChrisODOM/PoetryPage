a4e57c098e53b97c4afc4bbabf5a2ef4be6fff71
csodom <csodom22@gmail.com>
2024-05-12 00:17:43 -0700

    New background

--- src/App.js
+++ src/App.js
@@ -16,14 +16,15 @@ function App() {
     <div className="App">
       <div className="PoetrySection">
         <header className="App-header">
+        </header>
+        <br></br>
           <div className="title">
             <img height="64rem" width="64rem" src="/favicon.ico" alt="a green book"></img>
             @csodom Poetry
           </div>
-        </header>
-        <hr></hr>
+        <br></br>
           A home for all types of poems and important quotes, dated when I found them, and cited when possible.
-        <hr></hr>
+        <br></br>
         <h4>Filter Box</h4>
         <input className="filterBox" name="FilterBox" value={textFilter} onChange={handleChange} placeholder='Search the Cards below by Text' />
         {masterListOfPoetry.filter((currentValue) => {return currentValue.poetry.toLowerCase().includes(textFilter.toLowerCase())}).map((currentValue) => {return <PoetryCard date={currentValue.date} poetry={currentValue.poetry}/>})}
